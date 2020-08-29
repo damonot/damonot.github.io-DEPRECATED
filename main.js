aac0c43867a7ccd2d98f86de6d2529310a44d4e1
damonot <laptop+damonot@users.noreply.github.com>
2020-08-29 02:59:09 -0400

    Dynamic Padding, Width, & Centering

--- main.js
+++ main.js
@@ -6,3 +6,47 @@ light.addEventListener('click',
     bg.classList.toggle('body-dark');
   }
 )
+
+var windowWidth = window.innerWidth;
+var bio = document.getElementById("bio");
+var bioWidth = (300 / windowWidth) * 100;
+bio.style.width = bioWidth+"%";
+
+//var bioLeftMargin =  3 * windowWidth / 100;
+var bioLeftMargin =  (windowWidth / 35);
+bio.style.marginLeft = bioLeftMargin+"%";
+console.log("margin: "+bioLeftMargin)
+
+//update bio centering
+if((bioWidth >= 50)) {
+  bio.style.textAlign = "center";
+} else {
+  bio.style.textAlign = "right";
+}
+
+$(window).resize(function() {
+  // update window
+  windowWidth = window.innerWidth; 
+  //console.log("window: "+windowWidth);
+
+  // update bio width
+  bioWidth = (300 / windowWidth) * 100;
+  bio.style.width = bioWidth+"%";
+  //console.log("bio width: "+bioWidth);
+
+  //update bio margin
+  var bioLeftMargin =  (windowWidth / 35);
+  bio.style.marginLeft = bioLeftMargin+"%";
+  //console.log("margin: "+bioLeftMargin)
+
+  //update bio centering
+  if((bioWidth >= 50)) {
+    bio.style.textAlign = "center";
+  } else {
+    bio.style.textAlign = "right";
+  }
+});
+
+console.log(window.innerWidth);
+//var biowidth = (20 / screenwidth);
+//
